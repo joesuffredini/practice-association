37046518b9ba013babf56c66d9bc0202cda6b6e7
joesuffredini <gsuffred@gmail.com>
2021-03-15 23:55:35 -0400

    Adds validation to meeting and speaker models

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,7 @@
 class Meeting < ApplicationRecord
-  
-
+validates  :title, presence:true
+validates  :time,  presence:true 
+validates  :location, presence:true
+has_many  :meeting_speakers
+has_many  :speakers, through:  :meeting_speakers
 end
--- app/models/meeting_speaker.rb
+++ app/models/meeting_speaker.rb
@@ -1,2 +1,4 @@
 class MeetingSpeaker < ApplicationRecord
+  belongs_to :meetings
+  belongs_to :speakers
 end
